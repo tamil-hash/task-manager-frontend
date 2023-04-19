685e52094e8812bfeaeefe365e97c4d5e103e002
Tamil selvan S <tamil.selvans@raweng.com>
2023-04-19 15:34:20 +0530

    added prod url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,15 +1,17 @@
 interface ObjectKeys {
-    [key: string]: string | number;
+    [key: string]: string;
   }
 
   interface apiUrlsTypes extends ObjectKeys {
     local_dev: string;
     development: string;
+    production:string
   }
 
 export const apiUrls:apiUrlsTypes = {
     local_dev: "http://localhost:3000/",
     development: "https://task-manager-81mf.onrender.com/",
+    production: "https://task-manager-81mf.onrender.com/",
 }
 
 export const env:string = import.meta.env.MODE;
